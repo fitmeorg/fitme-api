ca9c412babac5cad375b3c0a3599a82a155506ce
Sirakusa <herrera.clip@gmail.com>
2024-08-29 01:09:53 -0600

    refactor: service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -26,58 +26,49 @@ export class AuthService {
   ) {}
 
   async register(registerDTO: RegisterDTO) {
-    try {
-      const { password, name, username, mail, ...optionalOptions } =
-        registerDTO;
+    const { password, name, username, mail, ...optionalOptions } = registerDTO;
 
-      await this.authRepository.failIfExist({
-        username,
-      });
+    await this.authRepository.failIfExist({
+      username,
+    });
 
-      const hash = await bcrypt.hash(password, SALT);
+    const hash = await bcrypt.hash(password, SALT);
 
-      const accessUser = await this.authRepository.create({
-        password: hash,
-        username,
-        mail,
-        ...optionalOptions,
-      });
+    const accessUser = await this.authRepository.create({
+      password: hash,
+      username,
+      mail,
+      ...optionalOptions,
+    });
 
-      const user = await this.userRepository.create({
-        name,
-        auth: accessUser._id,
-      });
+    const user = await this.userRepository.create({
+      name,
+      auth: accessUser,
+    });
 
-      this.eventEmitter.emitEvent('account.created', {
-        account: parseEntity(user),
-      });
+    this.eventEmitter.emitEvent('account.created', {
+      account: parseEntity(user),
+    });
 
-      return await this.tokenService.createUserToken({
-        id: user._id.toString(),
-        username: accessUser.username,
-        roles: [...accessUser.roles],
-      });
-    } catch (error) {
-      return error;
-    }
+    return await this.tokenService.createUserToken({
+      id: user._id.toString(),
+      username: accessUser.username,
+      roles: [...accessUser.roles],
+    });
   }
 
   async login(loginDTO: LoginDTO) {
-    try {
-      const access = await this.authRepository.findLogin(loginDTO);
+    const access = await this.authRepository.findLogin(loginDTO);
 
-      const user = await this.userRepository.findOneOrFail({
-        auth: access._id,
-      });
+    const user = await this.userRepository.findOneOrFail({
+      auth: access._id,
+    });
 
-      return await this.tokenService.createUserToken({
-        id: user._id.toString(),
-        username: access.username,
-        roles: [...access.roles],
-      });
-    } catch (error) {
-      return error;
-    }
+    return await this.tokenService.createUserToken({
+      id: user._id.toString(),
+      username: access.username,
+      roles: [...access.roles],
+    });
   }
 
   async refreshToken(req: any) {
@@ -87,19 +78,15 @@ export class AuthService {
   async authGoogle() {}
 
   async registerGoogle(req: any) {
-    try {
-      const access = await this.authRepository.create({
-        mail: req.user.email,
-        oauth: AuthProvider.Google,
-      });
+    const access = await this.authRepository.create({
+      mail: req.user.email,
+      oauth: AuthProvider.Google,
+    });
 
-      return this.userRepository.create({
-        name: req.user.name,
-        auth: access._id,
-      });
-    } catch (error) {
-      return error;
-    }
+    return this.userRepository.create({
+      name: req.user.name,
+      auth: access,
+    });
   }
 
   async loginGoogle(req: any) {
@@ -117,110 +104,94 @@ export class AuthService {
   }
 
   async googleAuthCallback(req: any, res: Response) {
-    try {
-      let user = await this.loginGoogle(req);
+    let user = await this.loginGoogle(req);
 
-      if (!user) {
-        user = await this.registerGoogle(req);
-      }
-
-      const access = await this.authRepository.findByIdOrFail(user.auth._id);
-
-      const token = await this.tokenService.createUserToken({
-        id: user._id.toString(),
-        username: `${req.user.firstName}`,
-        roles: [...access.roles],
-      });
-
-      res.cookie('access_token', token, {
-        maxAge: MAX_AGE,
-        sameSite: 'lax',
-        secure: false,
-      });
-
-      res.redirect(this.configService.get('googleOauth.redirectUrl'));
-    } catch (error) {
-      res.status(HttpStatus.BAD_REQUEST).send('Google authentication failed');
+    if (!user) {
+      user = await this.registerGoogle(req);
     }
+
+    const access = await this.authRepository.findByIdOrFail(user.auth._id);
+
+    const token = await this.tokenService.createUserToken({
+      id: user._id.toString(),
+      username: `${req.user.firstName}`,
+      roles: [...access.roles],
+    });
+
+    res.cookie('access_token', token, {
+      maxAge: MAX_AGE,
+      sameSite: 'lax',
+      secure: false,
+    });
+
+    res.redirect(this.configService.get('googleOauth.redirectUrl'));
   }
 
   async changePassword(changePassword: ChangePassword, req: any) {
-    try {
-      const token = req.cookies['access_token'];
+    const token = req.cookies['access_token'];
 
-      const userToken = await this.tokenService.validateToken(token);
+    const userToken = await this.tokenService.validateToken(token);
 
-      const access = await this.authRepository.findOneOrFail({
-        username: userToken.username,
-      });
+    const access = await this.authRepository.findOneOrFail({
+      username: userToken.username,
+    });
 
-      const comparePasswords = await bcrypt.compare(
-        changePassword.currentPassword,
-        access.password,
+    const comparePasswords = await bcrypt.compare(
+      changePassword.currentPassword,
+      access.password,
+    );
+
+    if (!comparePasswords) {
+      throw new HttpException(
+        'Current password is not equal',
+        HttpStatus.BAD_REQUEST,
       );
-
-      if (!comparePasswords) {
-        throw new HttpException(
-          'Current password is not equal',
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-
-      const hash = await bcrypt.hash(changePassword.newPassword, SALT);
-
-      await this.authRepository.update(access._id.toString(), {
-        password: hash,
-      });
-
-      return HttpStatus.OK;
-    } catch (error) {
-      return error;
     }
+
+    const hash = await bcrypt.hash(changePassword.newPassword, SALT);
+
+    await this.authRepository.update(access._id.toString(), {
+      password: hash,
+    });
+
+    return HttpStatus.OK;
   }
 
   async forgotPassword(forgotPassword: ForgotPassword) {
-    try {
-      const { mail } = forgotPassword;
-      const userAccess = await this.authRepository.findUserByMail(mail);
+    const { mail } = forgotPassword;
+    const userAccess = await this.authRepository.findUserByMail(mail);
 
-      if (!userAccess) {
-        throw new HttpException('User Not Found', HttpStatus.NOT_FOUND);
-      }
-
-      const user = await this.userRepository.findOneOrFail({
-        auth: userAccess._id,
-      });
-
-      const token = this.tokenService.createUserToken({
-        id: user._id.toString(),
-        username: user.name,
-        roles: [...userAccess.roles],
-      });
-
-      return token;
-    } catch (error) {
-      return error;
+    if (!userAccess) {
+      throw new HttpException('User Not Found', HttpStatus.NOT_FOUND);
     }
+
+    const user = await this.userRepository.findOneOrFail({
+      auth: userAccess._id,
+    });
+
+    const token = this.tokenService.createUserToken({
+      id: user._id.toString(),
+      username: user.name,
+      roles: [...userAccess.roles],
+    });
+
+    return token;
   }
 
   async changeForgotPassword(changePassword: ChangePassword, token: string) {
-    try {
-      const userToken = await this.tokenService.validateToken(token);
+    const userToken = await this.tokenService.validateToken(token);
 
-      const access = await this.authRepository.findOneOrFail({
-        username: userToken.username,
-      });
+    const access = await this.authRepository.findOneOrFail({
+      username: userToken.username,
+    });
 
-      const hash = await bcrypt.hash(changePassword.newPassword, SALT);
+    const hash = await bcrypt.hash(changePassword.newPassword, SALT);
 
-      await this.authRepository.update(access._id.toString(), {
-        password: hash,
-      });
+    await this.authRepository.update(access._id.toString(), {
+      password: hash,
+    });
 
-      return HttpStatus.OK;
-    } catch (error) {
-      return error;
-    }
+    return HttpStatus.OK;
   }
 
   logout() {
--- src/modules/images/images.service.ts
+++ src/modules/images/images.service.ts
@@ -14,68 +14,52 @@ export class ImagesService {
   ) {}
 
   async create(file: Array<Express.Multer.File>) {
-    try {
-      const idImage = [];
+    const idImage = [];
 
-      for (const image of file) {
-        const { width, height } = await this.getImageDimensions(image);
+    for (const image of file) {
+      const { width, height } = await this.getImageDimensions(image);
 
-        const fileDocument = await this.imageRepository.create({
-          url: `${this.configService.get('HOST_API')}/images/${image.destination.substring(2)}/${image.filename}`,
-          name: image.filename,
-          size: image.size,
-          format: image.mimetype,
-          height,
-          width,
-        });
+      const fileDocument = await this.imageRepository.create({
+        url: `${this.configService.get('HOST_API')}/images/${image.destination.substring(2)}/${image.filename}`,
+        name: image.filename,
+        size: image.size,
+        format: image.mimetype,
+        height,
+        width,
+      });
 
-        idImage.push(fileDocument._id);
-      }
-      return idImage;
-    } catch (error) {
-      return error;
+      idImage.push(fileDocument._id);
     }
+    return idImage;
   }
 
   async remove(idImage: string, pathImage: string) {
-    try {
-      const image = await this.imageRepository.findByIdOrFail(idImage);
+    const image = await this.imageRepository.findByIdOrFail(idImage);
 
-      const path = join(__dirname, `../../../${pathImage}`, image.name);
+    const path = join(__dirname, `../../../${pathImage}`, image.name);
 
-      if (!existsSync(path)) {
-        throw new BadRequestException('no product found');
-      }
-      unlinkSync(path);
-
-      await this.imageRepository.remove(image._id.toString());
-
-      return { message: 'File deleted successfully' };
-    } catch (error) {
-      throw new BadRequestException('Error deleting the file');
+    if (!existsSync(path)) {
+      throw new BadRequestException('no product found');
     }
+    unlinkSync(path);
+
+    await this.imageRepository.remove(image._id.toString());
+
+    return { message: 'File deleted successfully' };
   }
 
   async findImage(imageName: string, dest: string, res: Response) {
-    try {
-      const image = await this.imageRepository.findOneOrFail({
-        name: imageName,
-      });
+    const image = await this.imageRepository.findOneOrFail({
+      name: imageName,
+    });
 
-      const path = join(__dirname, `../../../${dest}`, image.name.toString());
+    const path = join(__dirname, `../../../${dest}`, image.name.toString());
 
-      res.sendFile(path);
-    } catch (error) {
-      return error;
-    }
+    res.sendFile(path);
   }
 
   async getImageDimensions(image: Express.Multer.File) {
-    try {
-      const dimensions = imageSize.imageSize(image.path);
-      return { width: dimensions.width, height: dimensions.height };
-    } catch (error) {
-      throw new Error('Could not get image dimensions');
-    }
+    const dimensions = imageSize.imageSize(image.path);
+    return { width: dimensions.width, height: dimensions.height };
   }
 }
